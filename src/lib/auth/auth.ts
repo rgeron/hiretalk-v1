4f07939722df62bd4752695a5fbd7e3903697e93
Melvyn M <56388157+Melvynx@users.noreply.github.com>
2024-02-07 08:22:04 +0800

    feat(nextauth): upgrade to v8

--- src/lib/auth/auth.ts
+++ src/lib/auth/auth.ts
@@ -1,10 +1,14 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
+import { SiteConfig } from "@/site-config";
 import { PrismaAdapter } from "@auth/prisma-adapter";
+import MagicLinkMail from "@email/MagicLinkEmail";
 import type { Session, User } from "next-auth";
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
-// import Resend from "next-auth/providers/resend";
+import Resend from "next-auth/providers/resend";
 import { env } from "../env";
+import { logger } from "../logger";
+import { sendEmail } from "../mail/sendEmail";
 import prisma from "../prisma";
 import { setupResendCustomer, setupStripeCustomer } from "./auth-config-setup";
 
@@ -13,23 +17,44 @@ export const {
   auth,
   // TODO : Use req callback
 } = NextAuth((req) => ({
-  // pages: {
-  //   signIn: "/auth/signin",
-  //   signOut: "/auth/signout",
-  //   error: "/auth/error", // Error code passed in query string as ?error=
-  //   verifyRequest: "/auth/verify-request", // (used for check email message)
-  //   newUser: "/auth/new-user", // New users will be directed here on first sign in (leave the property out if not of interest)
-  // },
+  pages: {
+    signIn: "/auth/signin",
+    signOut: "/auth/signout",
+    error: "/auth/error", // Error code passed in query string as ?error=
+    verifyRequest: "/auth/verify-request",
+    newUser: "/auth/new-user", // New users will be directed here on first sign in (leave the property out if not of interest)
+  },
   adapter: PrismaAdapter(prisma),
   providers: [
     GitHub({
       clientId: env.GITHUB_ID,
       clientSecret: env.GITHUB_SECRET,
     }),
+    Resend({
+      apiKey: env.RESEND_API_KEY,
+      sendVerificationRequest: async ({ identifier: email, url }) => {
+        const result = await sendEmail({
+          from: SiteConfig.email.from,
+          to: email,
+          subject: `Sign in to ${SiteConfig.domain}`,
+          react: MagicLinkMail({
+            url,
+          }),
+        });
 
+        if (result.error) {
+          logger.error("Auth Resend Provider Error", result.error);
+          throw new Error(`Failed to send email: ${result.error}`);
+        }
+      },
+    }),
     // 🔑 Add this line and the import to add credentials provider
     // getCredentialsProvider(),
   ],
+  session: {
+    strategy: "database",
+  },
+  secret: env.NEXTAUTH_SECRET,
   callbacks: {
     session(params) {
       if (params.newSession) return params.session;
