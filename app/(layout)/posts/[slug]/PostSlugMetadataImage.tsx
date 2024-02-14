c3df3d75d3f069e71f45d9c45fd6070f72b35c32
Melvyn M <56388157+Melvynx@users.noreply.github.com>
2024-02-14 10:58:02 +0800

    feat(blog): update post metadata image

---
+++ app/(layout)/posts/[slug]/PostSlugMetadataImage.tsx
@@ -0,0 +1,83 @@
+import type { Post } from "@/features/posts/post-manager";
+import { SiteConfig } from "@/site-config";
+import { alt } from "./opengraph-image";
+
+export type MetadataImageProps = {
+  post: Post;
+};
+
+export const PostSlugMetadataImage = ({ post }: MetadataImageProps) => {
+  return (
+    <div
+      style={{
+        width: "100%",
+        height: "100%",
+        display: "flex",
+        flexDirection: "column",
+        position: "relative",
+        gap: 16,
+        fontFamily: "Geist",
+        color: "white",
+        opacity: "1",
+        backgroundColor: "#090910",
+      }}
+    >
+      <div
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          right: 0,
+          bottom: 0,
+          opacity: "1",
+
+          borderRadius: 12,
+          margin: "4px",
+          backgroundColor: "#000000",
+        }}
+      ></div>
+      <img
+        src={post.attributes.coverUrl}
+        alt={alt}
+        style={{
+          width: "100%",
+          height: "100%",
+          objectFit: "cover",
+          borderRadius: 12,
+          filter: "blur(2px)",
+          position: "absolute",
+        }}
+      />
+
+      <div
+        style={{
+          position: "relative",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "flex-end",
+          alignItems: "flex-start",
+          height: "100%",
+          flex: 1,
+          paddingBottom: 32,
+          paddingLeft: 42,
+          gap: 16,
+          backdropFilter: "blur(12px)",
+        }}
+      >
+        <p
+          style={{
+            color: SiteConfig.brand.primary,
+            fontSize: "2rem",
+            fontWeight: "bold",
+            margin: 0,
+          }}
+        >
+          NOW.TS
+        </p>
+        <p style={{ fontSize: "3.5rem", fontWeight: "bold", margin: 0 }}>
+          {post.attributes.title}
+        </p>
+      </div>
+    </div>
+  );
+};
