00cff4bd2070d1e8a0b1e01e762d402ec7ffe91b
Melvyn M <56388157+Melvynx@users.noreply.github.com>
2024-04-09 07:29:59 +0800

    feat(providers): add automated provider

--- src/lib/auth/auth.ts
+++ src/lib/auth/auth.ts
@@ -1,17 +1,12 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { SiteConfig } from "@/site-config";
 import { PrismaAdapter } from "@auth/prisma-adapter";
-import MagicLinkMail from "@email/MagicLinkEmail";
 import type { User } from "@prisma/client";
 import type { Session } from "next-auth";
 import NextAuth from "next-auth";
-import GitHub from "next-auth/providers/github";
-import Resend from "next-auth/providers/resend";
 import { env } from "../env";
-import { logger } from "../logger";
-import { sendEmail } from "../mail/sendEmail";
 import { prisma } from "../prisma";
 import { setupResendCustomer, setupStripeCustomer } from "./auth-config-setup";
+import { getNextAuthConfigProviders } from "./getProviders";
 
 export const { handlers, auth: baseAuth } = NextAuth((req) => ({
   pages: {
@@ -23,34 +18,7 @@ export const { handlers, auth: baseAuth } = NextAuth((req) => ({
     // newUser: "/auth/new-user",
   },
   adapter: PrismaAdapter(prisma),
-  providers: [
-    GitHub({
-      clientId: env.GITHUB_ID,
-      clientSecret: env.GITHUB_SECRET,
-      // This is required because AuthJS beta is not stable.
-      allowDangerousEmailAccountLinking: true,
-    }),
-    Resend({
-      apiKey: env.RESEND_API_KEY,
-      sendVerificationRequest: async ({ identifier: email, url }) => {
-        const result = await sendEmail({
-          from: SiteConfig.email.from,
-          to: email,
-          subject: `Sign in to ${SiteConfig.domain}`,
-          react: MagicLinkMail({
-            url,
-          }),
-        });
-
-        if (result.error) {
-          logger.error("Auth Resend Provider Error", result.error);
-          throw new Error(`Failed to send email: ${result.error}`);
-        }
-      },
-    }),
-    // 🔑 Add this line and the import to add credentials provider
-    // getCredentialsProvider(),
-  ],
+  providers: getNextAuthConfigProviders(),
   session: {
     strategy: "database",
   },
--- src/lib/env.ts
+++ src/lib/env.ts
@@ -9,8 +9,10 @@ import { z } from "zod";
 export const env = createEnv({
   server: {
     DATABASE_URL: z.string().url(),
-    GITHUB_ID: z.string().min(1),
-    GITHUB_SECRET: z.string().min(1),
+    GITHUB_ID: z.string().optional(),
+    GITHUB_SECRET: z.string().optional(),
+    GOOGLE_ID: z.string().optional(),
+    GOOGLE_SECRET: z.string().optional(),
     RESEND_API_KEY: z.string().min(1),
     RESEND_AUDIENCE_ID: z.string().optional(),
     STRIPE_SECRET_KEY: z.string().min(1),
