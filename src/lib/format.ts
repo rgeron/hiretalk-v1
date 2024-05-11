e56d46bbd1d2e36f09951862c18f2025200cdec0
Melvyn M <56388157+Melvynx@users.noreply.github.com>
2024-05-11 10:03:49 +0800

    feat(format): remove useless format

--- src/lib/format.ts
+++
@@ -1,7 +0,0 @@
-export const formatPrice = (price: number | null = 0, currency = "eur") => {
-  const formatter = new Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency,
-  });
-  return formatter.format((price ?? 0) / 100);
-};
