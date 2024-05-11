d2d91e32bad1230aa3330ed003e623d087772552
Melvyn M <56388157+Melvynx@users.noreply.github.com>
2024-05-11 09:44:48 +0800

    feat(form): update submit button to haver better loading

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -19,6 +19,8 @@ const buttonVariants = cva(
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
         invert: "bg-foreground text-background hover:bg-foreground/90",
+        success: "bg-success text-success-foreground hover:bg-success/90",
+        warning: "bg-warning text-warning-foreground hover:bg-warning/90",
       },
       size: {
         default: "h-10 px-4 py-2",
--- src/features/form/SubmitButton.tsx
+++ src/features/form/SubmitButton.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { cn } from "@/lib/utils";
+import { motion } from "framer-motion";
 import type { ComponentPropsWithoutRef } from "react";
 import { useFormStatus } from "react-dom";
 import type { ButtonProps } from "../../components/ui/button";
@@ -18,17 +20,40 @@ export const SubmitButton = (props: ButtonProps) => {
 
 export const LoadingButton = ({
   loading,
+  children,
+  className,
   ...props
-}: ButtonProps & { loading?: boolean }) => {
+}: ButtonProps & {
+  loading?: boolean;
+  success?: string;
+}) => {
   return (
-    <Button {...props}>
-      {loading ? (
-        <>
-          <Loader className="mr-2" size={16} /> {props.children}
-        </>
-      ) : (
-        props.children
-      )}
+    <Button {...props} className={cn(className, "relative")}>
+      <motion.span
+        animate={{
+          opacity: loading ? 0 : 1,
+          y: loading ? -10 : 0,
+        }}
+      >
+        {children}
+      </motion.span>
+      <motion.span
+        initial={{
+          opacity: 0,
+          y: 10,
+        }}
+        animate={{
+          opacity: loading ? 1 : 0,
+          y: loading ? 0 : 10,
+        }}
+        exit={{
+          opacity: 0,
+          y: 10,
+        }}
+        className="absolute inset-0 flex items-center justify-center"
+      >
+        <Loader size={20} />
+      </motion.span>
     </Button>
   );
 };
