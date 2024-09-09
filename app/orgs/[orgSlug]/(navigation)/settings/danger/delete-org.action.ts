70cb87bed58023b5794ec6c2fafd2da387c04811
Matthieu BOURHIS <matthieu.bourhis@zai.fr>
2024-09-09 18:10:44 +0200

    Update delete-org.action.ts

--- app/orgs/[orgSlug]/(navigation)/settings/danger/delete-org.action.ts
+++ app/orgs/[orgSlug]/(navigation)/settings/danger/delete-org.action.ts
@@ -3,7 +3,6 @@
 import { orgAction } from "@/lib/actions/safe-actions";
 import { sendEmail } from "@/lib/mail/sendEmail";
 import { deleteOrganizationQuery } from "@/query/org/org-delete.query";
-import { SiteConfig } from "@/site-config";
 import OrgConfirmDeletionEmail from "@email/OrgConfirmDeletion.email";
 
 export const organizationDeleteAction = orgAction
@@ -12,7 +11,6 @@ export const organizationDeleteAction = orgAction
     await deleteOrganizationQuery(ctx.org.id);
 
     await sendEmail({
-      from: SiteConfig.email.from,
       subject: `Your organization has been deleted (${ctx.org.slug})`,
       to: ctx.user.email,
       react: OrgConfirmDeletionEmail({
