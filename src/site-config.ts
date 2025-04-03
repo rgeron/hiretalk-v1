625ee9eda5c5bf1b3c0f681a24974d03145f201d
melvynxdev <melvynmal@gmail.com>
2025-04-03 08:34:03 +0800

    make demo working

--- src/site-config.ts
+++ src/site-config.ts
@@ -1,8 +1,8 @@
 export const SiteConfig = {
   title: "NOW.TS",
   description: "Collect and showcase powerful video and text testimonials",
-  prodUrl: "https://nowts.app",
-  domain: "nowts.app",
+  prodUrl: "https://demo.nowts.app",
+  domain: "demo.nowts.app",
   appIcon: "/images/icon.png",
   company: {
     name: "NowTS",
