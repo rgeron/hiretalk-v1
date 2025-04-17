ac130b959696d111a5d6ec451ccefde7972030af
rgeron <raphaelgeron123@gmail.com>
2025-04-17 12:19:01 +0200

    sign in navigation

--- src/features/landing/hero.tsx
+++ src/features/landing/hero.tsx
@@ -33,7 +33,7 @@ export const Hero = () => {
             </Typography>
             <div className="mt-10 flex items-center justify-center gap-x-6">
               <ShimmerButton
-                href="/signin"
+                href="/auth/signin"
                 background="hsl(var(--primary))"
                 className="rounded-md px-3.5 py-2.5 text-sm font-semibold text-gray-700 shadow-sm hover:bg-gray-200 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-400"
               >
